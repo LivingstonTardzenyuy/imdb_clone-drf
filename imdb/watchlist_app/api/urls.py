2c186accf93cd62133bdce28b628dd8e36738bb2
kongnyuy livingston <kongnyuylivingston2@gmail.com>
2023-11-22 05:34:18 +0100

    performed CRUD operations using function based views

--- imdb/watchlist_app/api/urls.py
+++ imdb/watchlist_app/api/urls.py
@@ -3,4 +3,5 @@ from watchlist_app.api.views import *
 
 urlpatterns = [
     path('', home, name = 'home'),
+    path('movie/<int:pk>/', movie_details, name = 'movie-details'),
 ]
--- imdb/watchlist_app/api/views.py
+++ imdb/watchlist_app/api/views.py
@@ -1,8 +1,44 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from watchlist_app.models import Movie
+from watchlist_app.api.serializers import MovieSerializers
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def home(request):
-    movie = Movie.objects.all()
-    return JsonResponse(movie.data)
+    if request.method == 'GET':
+        movie = Movie.objects.all()
+        serializers = MovieSerializers(movie, many = True)
+        return Response(serializers.data)
+
+    if request.method == 'POST':
+        serializers = MovieSerializers(data = request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        else:
+            return Response(serializers.errors)
+
+
+@api_view(['GET', 'PUT'])
+def movie_details(request, pk):
+    if request.method == 'GET':
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except MovieDoesNotExists():
+            return Response(serializers.errors)
+        
+        serializers = MovieSerializers(movie)
+        return Response(serializers.data)
+
+    if request.method == 'PUT':
+        movie = Movie.objects.get(pk=pk)
+        serializers = MovieSerializers(movie, data= request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        else:
+            return Response(serializers.errors)
+
+
+
+
