ae7fd2142762a277df3bfb25087aea81f8f86947
kongnyuy livingston <kongnyuylivingston2@gmail.com>
2023-12-10 12:17:12 +0100

    connected my app

--- imdb/imdb/urls.py
+++ imdb/imdb/urls.py
@@ -4,7 +4,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('watchlist_app.api.urls')),
+    path('', include('watchlist_app.api.urls'),
+    path('account', include('user_app.api.urls')),
     # path('api-auth/', include('rest_framework.urls')),  # for login and logout views
 
 ]
