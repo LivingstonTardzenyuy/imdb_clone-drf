a4dfa1c194a8d5f3c1cbe5a68309fe67d2ee4795
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2023-12-14 12:32:26 +0100

    done with authomatic creation of tokens

--- imdb/user_app/api/views.py
+++ imdb/user_app/api/views.py
@@ -2,8 +2,12 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from user_app.api.serializers import RegistrationSerializer
 from rest_framework.authtoken.models import Token 
-from user_app import models
+# from user_app import models
 from rest_framework import status
+
+#jwt
+from rest_framework_simplejwt.tokens import RefreshToken
+
 
 @api_view(["POST"])
 def logout(request):
@@ -24,8 +28,17 @@ def registration_view(request):
             data['response'] = "Registration successful!"
             data['username'] = account.username             #we are using accounts b/c we stored it in our serializer
             data['email'] = account.email 
-            token = Token.objects.get(user = account).key 
-            data['token'] = token 
+            
+            #authomatic creation of tokens using Token base auth
+            # token = Token.objects.get(user = account).key 
+            # data['token'] = token 
+            
+            #authomatic creation of tokens using JWT base auth
+            refresh = RefreshToken.for_user(account)
+            data['token'] = {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token)
+                }
             
             # return Response(data, status=201)  # HTTP 201 Created for successful registration
         else:
