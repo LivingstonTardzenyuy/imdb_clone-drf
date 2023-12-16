d182cf96693e5bbb2776c1bdab8634cb80983a8f
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2023-12-16 02:47:24 +0100

    use custom page number pagination

--- imdb/watchlist_app/api/pagination.py
+++ imdb/watchlist_app/api/pagination.py
@@ -2,4 +2,7 @@ from rest_framework.pagination import PageNumberPagination
 
 class WatchListPagination(PageNumberPagination):
     page_size = 5
-    
+    page_query_param = 'watchlist'
+    page_size_query_param = '10'
+    max_page_size = '20'
+    last_page_strings = 'end'
