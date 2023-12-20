469632f34eb2a4784ceed7d181d49d4243bf1877
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2023-12-20 16:23:25 +0100

    added login and logout test cases

--- imdb/user_app/tests.py
+++ imdb/user_app/tests.py
@@ -16,3 +16,29 @@ class RegisterTestCase(APITestCase):
         }
         response = self.client.post(reverse('register'), data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+    
+class LoginLogoutTestCase(APITestCase):
+    
+    def setUp(self):        #creating a new user
+        self.user = User.objects.create_user(
+            username = "example",
+            password = "NewPassword123"
+        )
+        
+    def test_login(self):
+        data = {                    #login in a user
+            "username": "example",
+            "password": "NewPassword123"
+        }
+        
+        response = self.client.post(reverse('login'), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        
+    def test_logout(self):
+        self.token = Token.objects.get(user__username = "example")      #obtaining the token. 
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+        response = self.client.post(reverse('logout'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
