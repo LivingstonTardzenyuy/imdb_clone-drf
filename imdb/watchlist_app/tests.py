bbffc165900f37c2a03f96afcf665ecd4c5810f2
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2023-12-22 14:23:08 +0100

    working on streamplatformtesting

--- imdb/watchlist_app/tests.py
+++ imdb/watchlist_app/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from rest_framework.test import force_authenticate
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from django.urls import reverse
+from django.contrib.auth.models import User 
 
-# Create your tests here.
+from watchlist_app.api import serializers 
+from watchlist_app import models
+
+class StreamPlatformTestCase(APITestCase):
+    def test_streamPlatFormCreate(self):
+        data = {
+            "name": "kongyuylivingston",
+            "about": "best movie on earth",
+            "website": "www.kongnyuy.com",
+        }
+        
+    response = self.client.post(reverse('streamplatform-list'), data)
+    self.assertEqual(response.status_code, status = status.HTTP_201_CREATED)
