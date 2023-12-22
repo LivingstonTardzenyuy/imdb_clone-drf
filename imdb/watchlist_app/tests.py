ecc2ff4ce6bab6cc2c8f5c3ed2d9e4f7db503507
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2023-12-22 14:31:59 +0100

    allowed users to make a get request from StreamPlatForm

--- imdb/watchlist_app/tests.py
+++ imdb/watchlist_app/tests.py
@@ -10,6 +10,13 @@ from watchlist_app.api import serializers
 from watchlist_app import models
 
 class StreamPlatformTestCase(APITestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create(username = "example", password = "Password@123")
+        #Login in a user. 
+        self.token = Token.objects.get(user__username = self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        
     def test_streamPlatFormCreate(self):
         data = {
             "name": "kongyuylivingston",
@@ -18,4 +25,8 @@ class StreamPlatformTestCase(APITestCase):
         }
         
         response = self.client.post(reverse('streamplatform-list'), data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_streamplatform_list(self):
+        response = self.client.get(reverse('streamplatform-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
