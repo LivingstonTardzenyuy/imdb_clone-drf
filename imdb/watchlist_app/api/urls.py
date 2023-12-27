2c40f7a227a712d95280cb107ee299e2ec899410
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2023-12-27 04:18:25 +0100

    working on reviews

--- imdb/watchlist_app/api/urls.py
+++ imdb/watchlist_app/api/urls.py
@@ -7,8 +7,8 @@ router = DefaultRouter()
 router.register('stream', StreamPlatFormAV, basename = 'streamplatform')
 
 urlpatterns = [
-    path('', WatchListList.as_view(), name = 'home'),
-    path('watchlist/<int:pk>/', WatchListDetails.as_view(), name = 'watchlist-details'),
+    path('', WatchListList.as_view(), name = 'movie-list'),
+    path('watchlist/<int:pk>/', WatchListDetails.as_view(), name = 'movie-detail'),
     path('list2', WatchListAV.as_view(), name='movie-details'),
 
 # ReviewsListDetails
--- imdb/watchlist_app/tests.py
+++ imdb/watchlist_app/tests.py
@@ -39,7 +39,68 @@ class StreamPlatformTestCase(APITestCase):
         
         
     def test_streamplatform_id(self):
-        response = self.client.get(reverse('streamplatform-detail', args = (self.stream.id,)))
+        response = self.client.get(reverse('streamplatform-detail', args = (self.stream.id,)))      #args allow me to access the individual elements. 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    
+    # def test_stramplatform
+class WatchlistTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="kongnyuy", password="Password@123")
+        self.token = Token.objects.get(user__username = self.user)
+        self.client.credentials(HTTP_AUTHORIZATION = 'Token' + self.token.key)
+        
+        self.stream = StreamPlatForm.objects.create(
+            name="Netflix",
+            about="The movie of the gold",
+            website="www.abakwa.com"
+        
+        )
+        
+        self.watchlist = models.WatchList.objects.create(platform = self.stream, title="example movies", description="Example movie", active=True)
+    
+    def test_watchlist_create(self):
+        data = {
+            "title": "Abakwa",
+            "description": "the movie that will be great for all",
+            "platform": self.stream,
+            "active": True 
+        }
+        response = self.client.post(reverse('movie-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        
+    def test_watchlist_list(self):
+        response = self.client.get(reverse('movie-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_watchlist_ind(self):
+        response = self.client.get(reverse('movie-detail', args = (self.watchlist.id,)))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(models.WatchList.objects.get().title, "example movies")
+        self.assertEqual(models.WatchList.objects.count(), 1)
+        
+    # def test_watchlist_delete(self):
+    #     response = self.client.delete(reverse('movie-detail', args=(self.watchlist.id,)))
+    #     self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+class ReviewTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="example", password="Password@123")
+        self.token = Token.objects.get(user__username = self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token' + self.token.key)
+        
+        self.stream = models.StreamPlatForm.objects.create(
+            name="Netflix",
+            about="#1 platform",
+            website="www.abakwa.com"
+        )
+        
+        self.watchlist = models.WatchList.objects.create(
+            platform = self.stream,
+            title = 'the pride of abakwa',
+            description = 'best movie in the city',
+            active = 'True'
+        )
+        
         
         
