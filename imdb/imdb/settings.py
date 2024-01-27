1c4127bec31caed790c4d759fae427e370582e2d
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-01-27 02:45:33 +0100

    added files for deployment

--- imdb/imdb/settings.py
+++ imdb/imdb/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -183,3 +183,13 @@ SPECTACULAR_SETTINGS = {
     'DESCRIPTION': 'This is the clone of IMDB using DRF',
     # 'VERSION': '1.0.0',
 }
+
+
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- imdb/imdb/urls.py
+++ imdb/imdb/urls.py
@@ -2,6 +2,9 @@
 from django.contrib import admin
 from django.urls import path, include 
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,3 +18,7 @@ urlpatterns = [
     # path('api-auth/', include('rest_framework.urls')),  # for login and logout views
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
